77d9ca1ea0baeeca6d304c9e9a6dd5f780aa2859
Goce Mitevski <goce.mitevski@keitaro.com>
2019-05-01 16:10:23 +0200

    Collapse navbar on select

--- assets/js/content.js
+++ assets/js/content.js
@@ -10,6 +10,39 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+/* Not ready yet!
+
+var deferredPrompt;
+var btnAdd = document.querySelector('.btn-install');
+
+window.addEventListener('beforeinstallprompt', function (e) {
+
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+
+  console.log(e);
+
+  // Stash the event so it can be triggered later
+  deferredPrompt = e;
+
+  btnAdd.style.display = 'block';
+
+});
+
+btnAdd.addEventListener('click', function (e) {
+
+  deferredPrompt.prompt();
+
+  deferredPrompt.userChoice.then(function (choiceResult) {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    }
+    deferredPrompt = null;
+  });
+
+});
+*/
+
 $(document).ready(function (e) {
 
   var audio = document.getElementById("audio");
@@ -90,6 +123,8 @@ function bindLinks() {
       modalDestroy();
 
     });
+
+    $('#navbarSupportedContent').collapse('hide');
 
   });
 
