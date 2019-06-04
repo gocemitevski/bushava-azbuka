ad76a6c5906cb44d939ca4a4b24016c1153d3d2c
Goce Mitevski <goce.mitevski@keitaro.com>
2019-06-04 21:31:16 +0200

    Refactor content.js

--- assets/js/content.js
+++ assets/js/content.js
@@ -59,53 +59,21 @@ $(document).ready(function (e) {
     }
   }*/
 
-  $(audio).on("click", function (e) {
-
-    if (video.muted) {
-      video.muted = false;
-      localStorage.setItem('isUnmuted', 1);
-    } else {
-      video.muted = true;
-      localStorage.setItem('isUnmuted', 0);
-    }
-
-    audioIconToggle();
-
-  });
-
-  modalCreate();
-  modalDestroy();
-  bindLinks();
-
-});
-
-$(window).on("popstate", function (e) {
-
-  // When the browser goes back replace the content and title
-  $('title').html(e.originalEvent.state.title);
-  $('#content').removeClass('empty').html(e.originalEvent.state.content);
-
-})
-
-function bindLinks() {
-
-  $(".navbar-nav a.nav-link[href^='/']").on('click', function (e) {
+  $(".navbar-nav .nav-link").on('click', function (e) {
 
     // Stop link from activating
     e.preventDefault();
 
-    $('.navbar-nav .nav-item').removeClass('active');
-
-    $(this).parent().addClass('active');
+    markNavItem($(this));
 
     // Get the URL to load
     url = $(this).attr('href');
 
     // Send a Get request to the URL
     $.get(url, function (data) {
+
       // Get the title of the new page
-      // var regex = RegExp('/<title>(.*)<\/title>/', 'g');
-      var newTitle = '';
+      var newTitle = $(data).filter('title').html();
 
       // Set the title to the new title
       $('title').html(newTitle);
@@ -127,6 +95,48 @@ function bindLinks() {
     $('#navbarSupportedContent').collapse('hide');
 
   });
+
+  $(audio).on("click", function (e) {
+
+    if (video.muted) {
+      video.muted = false;
+      localStorage.setItem('isUnmuted', 1);
+    } else {
+      video.muted = true;
+      localStorage.setItem('isUnmuted', 0);
+    }
+
+    audioIconToggle();
+
+  });
+
+  modalCreate();
+  modalDestroy();
+
+});
+
+$(window).on("popstate", function (e) {
+
+  // When the browser goes back replace the content and title
+  $('title').html(e.originalEvent.state.title);
+  $('#content').removeClass('empty').html(e.originalEvent.state.content);
+
+  $.each($('.navbar-nav .nav-link'), function (key, el) {
+
+    if ($(el).attr('href') == e.originalEvent.currentTarget.location.pathname) {
+      $(el).parent().addClass('active');
+    } else {
+      $(el).parent().removeClass('active');
+    }
+
+  });
+
+});
+
+function markNavItem(item) {
+
+  $('.navbar-nav .nav-item').removeClass('active');
+  item.parent().addClass('active');
 
 }
 
