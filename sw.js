f3fd78ff672ff7ab6b8f87c42e123ffb3aea652b
Goce Mitevski <goce.mitevski@keitaro.com>
2019-11-30 15:31:37 +0100

    Replace hard-coded cache version name

--- sw.js
+++ sw.js
@@ -89,7 +89,7 @@ self.addEventListener('fetch', (event) => {
     caches.match(event.request).then((resp) => {
       return resp || fetch(event.request).then((response) => {
         let responseClone = response.clone();
-        caches.open('v1').then((cache) => {
+        caches.open(cacheName).then((cache) => {
           cache.put(event.request, responseClone);
         });
 
