5af91c2a1c86d2b684e50d66f2bfc0c990a16ddb
Goce Mitevski <goce.mitevski@keitaro.com>
2022-02-13 12:43:27 +0100

    Update Service Worker fetch event

--- sw.js
+++ sw.js
@@ -117,19 +117,33 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((resp) => {
+    caches.match(event.request).then((response) => {
       // Cache hit - return response
-      if (resp) {
-        return resp;
+      if (response) {
+        return response;
       }
-      return fetch(event.request);
 
-      // return resp || fetch(event.request).then((response) => {
-      //   return caches.open(cacheName).then((cache) => {
-      //     cache.put(event.request, response.clone());
-      //     return response;
-      //   });
-      // });
+      return fetch(event.request).then(
+        function(response) {
+          // Check if we received a valid response
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // IMPORTANT: Clone the response. A response is a stream
+          // and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need
+          // to clone it so we have two streams.
+          var responseToCache = response.clone();
+
+          caches.open(cacheName)
+            .then(function(cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        }
+      );
     })
   );
 });
